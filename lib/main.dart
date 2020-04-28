36a529caa0c46cdb402192cebfb942a2b33684d3
lily <614720105@qq.com>
2020-04-28 21:39:04 +0800

    time :2020.04.28 content:navigation,界面传值。

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FirstScreen(),
+      home: new TodoScreen(),
+//      home: FirstScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -28,11 +29,12 @@ class FirstScreen extends StatelessWidget{
       ),
       body: new Center(
         child: new RaisedButton(
-        child: new Text('Launch new sreen'),
-            onPressed: (){
-          Navigator.push(
-              context,
-              new MaterialPageRoute(builder: (context) => new SecondScreen()),);
+            child: new Text('Launch new sreen'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(builder: (context) => new SecondScreen()),
+              );
             }),
       ),
     );
@@ -40,7 +42,7 @@ class FirstScreen extends StatelessWidget{
   }
 }
 
-class SecondScreen extends StatelessWidget{
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -49,14 +51,68 @@ class SecondScreen extends StatelessWidget{
       ),
       body: new Center(
         child: new RaisedButton(
-          child: new Text("Sencond Button"),
-            onPressed: (){
+            child: new Text("Sencond Button"),
+            onPressed: () {
               Navigator.pop(context);
             }),
       ),
     );
     throw UnimplementedError();
   }
-
 }
 
+class Todo {
+  final String title;
+  final String description;
+  Todo(this.title, this.description);
+}
+
+class TodoScreen extends StatelessWidget {
+  final todos = new List.generate(
+      20,
+      (i) => new Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          ));
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todos'),
+      ),
+      body: new ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text(todos[index].title),
+              onTap: () {
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) {
+                  return new DetailScreen(todo: todos[index]);
+                }));
+              },
+            );
+          }),
+    );
+    throw UnimplementedError();
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final todo;
+  DetailScreen({Key key, this.todo}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('${todo.title}'),
+      ),
+      body: new Padding(
+        padding: new EdgeInsets.all(16.0),
+        child: new Text('${todo.description}'),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
